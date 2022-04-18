96a645548c3544ba79e17eafd0a091a3563243fa
Angel Velasquez <pcisavel@upc.edu.pe>
2022-04-18 08:02:22 -0500

    feat: Added Unavailable Content Section  Component for Error handling Scenario.

---
+++ src/components/unavailable-content.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row class="flex-column text-center">
+    <v-col class="text-h4">Unavailable News</v-col>
+    <v-col v-for="error in errors" class="text-h6">{{ error }}</v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "unavailable-content",
+  props: {
+    errors: null
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
