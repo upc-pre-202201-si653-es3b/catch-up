9e2bb8417c6a408720453c434078eee7378ac59e
Angel Velasquez <pcisavel@upc.edu.pe>
2022-04-18 08:44:13 -0500

    feat: Added Side Content and Main Content Section Components.

---
+++ src/components/main-content.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col v-for="article in articles" :key="article.title">
+        <article-card :article="article"/>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import ArticleCard from "./article-card.vue";
+export default {
+  name: "main-content",
+  components: {ArticleCard},
+  props: {
+    articles: Array
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
