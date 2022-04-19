ede383284e15849a3681ef62a0c45543cd96caf1
Angel Velasquez <pcisavel@upc.edu.pe>
2022-04-19 10:10:27 -0500

    fix: Modified import for vuei18n at vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from '@vuetify/vite-plugin'
-import {vueI18n} from "@intlify/vite-plugin-vue-i18n/lib/index";
+import {vueI18n} from "@intlify/vite-plugin-vue-i18n";
 
 const path = require('path')
 
